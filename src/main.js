71852d114a34844a7ae9bc4b1dae634db56b224b
vvs <1438102025@qq.com>
2021-10-05 17:01:05 +0800

    :fire:  :zap:  :art: :bug:  放入github

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 
+Vue.use(Vant)
+//关掉控制台中的 '请使用production版本的vue提示'
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/untils/request.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+// 调用 axios.create() 方法，创建 axios 的实例对象
+export default  axios.create({
+    // 请求根路径
+    baseURL: 'http://www.liulongbin.top:8000'
+  })
+  
